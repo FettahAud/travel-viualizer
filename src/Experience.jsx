5e1977b81b68f3fe23573f348a308974a2844b47
fettahaud <fettahaud@gmail.com>
2023-02-16 18:00:41 +0300

    Done bases

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,122 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import * as THREE from "three";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { useMemo } from "react";
+import vertex from "./shaders/vertex.glsl";
+import fragment from "./shaders/fragment.glsl";
+import { useFrame } from "@react-three/fiber";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const earth = useTexture("./earth.jpg");
+  const pointGeo = new THREE.SphereGeometry(0.02, 20, 20);
+  const pointMat = new THREE.MeshBasicMaterial({ color: "red" });
+  const lineMat = new THREE.ShaderMaterial({
+    vertexShader: vertex,
+    fragmentShader: fragment,
+    uniforms: {
+      time: { value: 0 },
+    },
+  });
 
-        <Perf position="top-left" />
+  const coordinates = [
+    {
+      lat: 41.0082,
+      lng: -28.9784,
+    },
+    {
+      lat: 26.8206,
+      lng: -30.8025,
+    },
+    {
+      lat: 19.683139,
+      lng: 155.539693,
+    },
+    {
+      lat: 34.0522,
+      lng: 118.2437,
+    },
+    {
+      lat: 37.5753,
+      lng: -36.9228,
+    },
+  ];
 
-        <OrbitControls makeDefault />
+  const getCoordinates = ({ lat, lng }) => {
+    const phi = (90 - lat) * (Math.PI / 180);
+    const theta = (lng + 180) * (Math.PI / 180);
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+    const x = Math.sin(phi) * Math.cos(theta);
+    const z = Math.sin(phi) * Math.sin(theta);
+    const y = Math.cos(phi);
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+    return { x, y, z };
+  };
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+  const getCurve = (p1, p2) => {
+    const v1 = new THREE.Vector3(p1.x, p1.y, p1.z);
+    const v2 = new THREE.Vector3(p2.x, p2.y, p2.z);
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+    const curvePoints = [];
 
+    for (let i = 0; i < 20; i++) {
+      let p = new THREE.Vector3().lerpVectors(v1, v2, i / 19);
+      p.normalize();
+
+      p.multiplyScalar(1 + 0.1 * Math.sin(Math.PI * (i / 20)));
+      curvePoints.push(p);
+    }
+    const path = new THREE.CatmullRomCurve3(curvePoints);
+    return path;
+  };
+
+  const positionsAndTubes = useMemo(() => {
+    const positions = [];
+    const tubes = [];
+
+    coordinates.forEach((item, i, arr) => {
+      const pos = getCoordinates(item);
+      const pos2 = i != arr.length - 1 ? getCoordinates(arr[i + 1]) : null;
+
+      if (pos2) {
+        tubes.push(
+          <mesh key={i} material={lineMat}>
+            <tubeGeometry args={[getCurve(pos, pos2), 20, 0.005, 8, false]} />
+          </mesh>
+        );
+      }
+
+      positions.push(pos);
+    });
+
+    return { positions, tubes };
+  }, [coordinates]);
+
+  useFrame(({ clock }) => {
+    lineMat.uniforms.time.value = clock.elapsedTime;
+  });
+
+  return (
+    <>
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh>
+        <icosahedronGeometry args={[1, 10]} />
+        <meshStandardMaterial map={earth} />
+      </mesh>
+      {positionsAndTubes.positions.map((pos, i) => (
+        <mesh
+          geometry={pointGeo}
+          material={pointMat}
+          position={[pos.x, pos.y, pos.z]}
+          key={i}
+        />
+      ))}
+      {positionsAndTubes.tubes}
     </>
+  );
 }
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,7 +2,12 @@
 precision mediump float;
 #endif
 
+varying vec2 vUv;
+uniform float time;
+
 void main(){
-    vec3 color = vec3(0.0);
-    gl_FragColor = vec4(color, 1.0);
+    float dash = sin(vUv.x*50. - time * 10.);
+    if( dash < 0.) discard;
+
+    gl_FragColor = vec4(vUv.x, 0., 0., 1.0);
 }
